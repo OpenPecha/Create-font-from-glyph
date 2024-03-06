db16d32d5e2f065c9be6f67f2fe28934b31d121d
Tenzin Kalden <tenkalden007@gmail.com>
2024-03-06 15:44:03 +0530

    module_update

--- create_font_from_glyph/create_font.py
+++ create_font_from_glyph/create_font.py
@@ -14,24 +14,16 @@ import numpy as np
 import jsonlines
 import logging
 import traceback
+import re
+import cv2
 
 s3 = monlam_ai_ocr_s3_client
 bucket_name = MONLAM_AI_OCR_BUCKET
 
 logging.basicConfig(filename='skipped_glyph.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
-# convert png to svg
-
-
-def png_to_svg(input_png, output_svg):
-    image = Image.open(input_png).convert('1')
-    pbm_path = "temp.pbm"
-    image.save(pbm_path)
-    subprocess.run(["potrace", pbm_path, "-s", "--scale", "5.5", "-o", output_svg])
-    os.remove(pbm_path)
 
 # get image path from url
-
 
 def get_image_path(image_url):
     image_parts = (image_url.split("?")[0]).split("/")
@@ -39,17 +31,74 @@ def get_image_path(image_url):
     decoded_key = urllib.parse.unquote(obj_key)
     image_name = decoded_key.split("/")[-1]
 
+    # Extracting file name and extension
+    file_name, file_extension = os.path.splitext(image_name)
+
+    # Removing underscore and number after the file name
+    image_name_without_suffix = re.sub(r'(_\d+)$', '', file_name)
+
+    # Keeping only Tibetan Unicode characters
+    image_name_tibetan_only = re.sub(r'[^\u0F00-\u0FFF]', '', image_name_without_suffix)
+
     try:
         response = s3.get_object(Bucket=bucket_name, Key=decoded_key)  # download the image from the bucket
         image_data = response['Body'].read()
-        with open(fr"data/downloaded_glyph/{image_name}", 'wb') as f:
+        with open(fr"data/downloaded_glyph/{image_name_tibetan_only}{file_extension}", 'wb') as f:
             f.write(image_data)
 
     except Exception as e:
         print(fr"Error while downloading {image_name} due to {e}")
-    return fr"data/downloaded_glyph/{image_name}"
+    return fr"data/downloaded_glyph/{image_name_tibetan_only}{file_extension}"
 
-# convert outside polygon to white
+
+
+
+def get_image_output_path(image, image_name, output_path, headlines):
+    image_width = image.width
+    headline_starts = headlines["headline_starts"]
+    headline_ends = headlines["headline_ends"]
+
+    glyph_name = (image_name.split(".")[0]).split("_")[0]
+
+    left_edge, right_edge = get_edges(image)
+    if left_edge is None:
+        return None
+    # <Unicode>-<PNG width - margins>-<baseline start - left glyph edge>-<right glyph edge - baseline end>.png
+    new_image_name = f"{glyph_name}_{int(image_width - (left_edge + right_edge))}_{int(headline_starts - left_edge)}_{int(headline_ends - right_edge)}.png"
+    print(new_image_name)
+    image_output_path = f"{output_path}/{new_image_name}"
+    print(image_width)
+    return image_output_path
+
+# for left and right edges
+def get_edges(image):
+    if isinstance(image, np.ndarray):  # Check if the input is a numpy array
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    elif isinstance(image, Image.Image):  # Assuming Image is imported from PIL
+        if image.mode != 'L':
+            image = image.convert('L')
+        gray_image = np.array(image)
+    else:
+        raise ValueError("Unsupported image type")
+
+    edges = cv2.Canny(gray_image, 50, 150)  # Adjust the thresholds as needed
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    left_edge = min(contours[0][:, 0, 0]) if contours else None
+    right_edge = max(contours[0][:, 0, 0]) if contours else None
+    print(left_edge,right_edge)
+    
+    return left_edge, right_edge
+
+# for headlines
+def get_headlines(baselines_coord):
+    min_x = min(coord[0] for coord in baselines_coord)
+    max_x = max(coord[0] for coord in baselines_coord)
+    headlines = {
+        "headline_starts": min_x,
+        "headline_ends": max_x
+    }
+    print(headlines)
+    return headlines
 
 
 def convert_outside_polygon_to_white(image_path, span, output_path):
@@ -60,58 +109,24 @@ def convert_outside_polygon_to_white(image_path, span, output_path):
             baselines_coord = info["points"]
         if info["label"] == "Glyph":
             polygon_points = [(x, y) for x, y in info["points"]]
-    if baselines_coord == None or polygon_points == None:
+    if baselines_coord is None or polygon_points is None:
         return None
+
     image = Image.open(image_path)
     mask = Image.new("L", image.size, 0)
     draw = ImageDraw.Draw(mask)
     draw.polygon(polygon_points, fill=255)
     result_image = Image.new("RGB", image.size, (255, 255, 255))
     result_image.paste(image, mask=mask)
+
     headlines = get_headlines(baselines_coord)
-    image_output_path = f"{output_path}/{image_path.split('/')[-1]}"
+    image_output_path = get_image_output_path(image, image_path.split('/')[-1], output_path, headlines)
+
     if image_output_path is not None:
         result_image.save(image_output_path)
         return image_output_path
-
-# for headlines
-
-
-def get_headlines(baselines_coord):
-    min_x = min(coord[0] for coord in baselines_coord)
-    max_x = max(coord[0] for coord in baselines_coord)
-    headlines = {
-        "top_left_x": min_x,
-        "top_right_x": max_x
-    }
-    return headlines
-
-# for left and right edges
-
-
-def get_edges(image):
-    if image.mode != 'L':
-        image = image.convert('L')
-    image_array = np.array(image)
-    left_edge = None
-    right_edge = None
-    for x in range(image_array.shape[1]):
-        for y in range(image_array.shape[0]):
-            if image_array[y, x] == 0:
-                left_edge = x
-                break
-        if left_edge is not None:
-            break
-    for x in range(image_array.shape[1] - 1, -1, -1):
-        for y in range(image_array.shape[0]):
-            if image_array[y, x] == 0:
-                right_edge = x
-                break
-        if right_edge is not None:
-            break
-    return left_edge, right_edge
-
-# finding the bounding box
+    else:
+        return None
 
 
 def find_glyph_bbox(image):
@@ -128,88 +143,51 @@ def find_glyph_bbox(image):
                 lower = max(lower, y)
     return left, upper, right + 1, lower + 1
 
-# create sgv
 
-
-def create_svg_with_glyph(png_path, output_svg_path):
+def create_svg_with_glyph(width, height, left, upper, right, lower, png_base64, output_svg):
     try:
-        with Image.open(png_path) as img:
+        dwg = svgwrite.Drawing(output_svg, profile='tiny', size=(f'{width}px', f'{height}px'))
+        image = svgwrite.image.Image(href=f'data:image/png;base64,{png_base64}',
+                                     insert=(left, upper),
+                                     size=(right - left, lower - upper))
+        dwg.add(image)
+        dwg.save()
+    except Exception as e:
+        print(f"Error creating SVG with glyph: {e}")
+
+
+
+def png_to_svg(input_png, output_svg):
+    try:
+        image = Image.open(input_png).convert('1')
+        pbm_path = "temp.pbm"
+        image.save(pbm_path)
+        subprocess.run(["potrace", pbm_path, "-s", "--scale", "5.5", "-o", output_svg],encoding='utf-8')
+        os.remove(pbm_path)
+
+        with Image.open(input_png) as img:
             width, height = img.size
             left, upper, right, lower = find_glyph_bbox(img)
             png_base64 = base64.b64encode(img.tobytes()).decode('utf-8')
-            dwg = svgwrite.Drawing(output_svg_path, profile='tiny', size=(f'{width}px', f'{height}px'))
-            image = svgwrite.image.Image(href=f'data:image/png;base64,{png_base64}')
-            image['x'] = f'{left}px'
-            image['y'] = f'{upper}px'
-            image['width'] = f'{right - left}px'
-            image['height'] = f'{lower - upper}px'
-            dwg.add(image)
-            dwg.save()
+        
+        create_svg_with_glyph(width, height, left, upper, right, lower, png_base64, output_svg)
     except Exception as e:
-        print(f"Error creating SVG with glyph: {e}")
+        print(f"Error converting PNG to SVG: {e}")
+
 
 # create font into desired format
 
 
-def create_into_desired_format(cleaned_image_path):
-    try:
-        image_name = os.path.basename(cleaned_image_path)
-        input_dir = "data/cleaned_images"
-        output_ufo = f"data/ufo/{image_name}.ufo"
-        output_ttf = f"data/ttf/{image_name}.ttf"
-        font_file = "data/font/font.ttf"
-        font_size = 12
-        font_name = "My Font"
-
-        font = UFOWriter(font_file)
-        ttf_font = ImageFont.truetype(font_file, font_size)
-
-        for filename in os.listdir(input_dir):
-            if filename.endswith(".png"):
-                unicode_value, width, lsb, rsb = filename[:-4].split("-")
-                unicode_value = int(unicode_value, 16)
-                width = int(width)
-                lsb = int(lsb)
-                rsb = int(rsb)
-                image = Image.open(os.path.join(input_dir, filename))
-                glyph_pen = TTGlyphPen(font)
-                glyph_pen.moveTo((0, 0))
-                glyph_pen.lineTo((width, 0))
-                glyph_pen.lineTo((width, image.height))
-                glyph_pen.lineTo((0, image.height))
-                glyph_pen.closePath()
-                glyph = glyph_pen.glyph()
-                glyph.width = width
-                glyph.leftMargin = lsb
-                glyph.rightMargin = rsb
-                glyph.font = font_name
-                glyph.font_size = font_size
-                font.newGlyph(hex(unicode_value)[2:].upper())
-                font.glyphs[hex(unicode_value)[2:].upper()].fromTTGlyph(glyph)
-
-        ttf_font_metadata = {"name": {"fontFamily": font_name}}
-        ttf_font.set(**ttf_font_metadata)
-
-        font.writeToFile(output_ufo)
-        ttf_font = TTFont(output_ufo)
-        ttf_font.save(output_ttf)
-
-        print(f"Font created successfully: {output_ttf}")
-
-    except Exception as e:
-        print(f"Error creating font: {e}")
-
-
 def main():
     jsonl_paths = list(Path("derge/glyph_ann_reviewed_batch6_ga").iterdir())
-    processed_ids = set()  
+    processed_ids = set()
     for jsonl_path in jsonl_paths:
         try:
             with jsonlines.open(jsonl_path) as reader:
                 for line in reader:
                     if line["answer"] == "accept":
                         try:
-                            image_id = line["id"].split("_")[0] 
+                            image_id = line["id"].split("_")[0]
                             if image_id in processed_ids:
                                 logging.info(f"Skipping duplicate ID: {image_id}")
                                 continue
@@ -222,9 +200,10 @@ def main():
                             if cleaned_image_path is None:
                                 logging.info(f"Skipping {image_path}")
                                 continue
-                            filename = Path(cleaned_image_path).stem
-                            output_path = Path(f"data/glyph_images/{filename}.svg")
+                            filename = (cleaned_image_path.split("/")[-1]).split(".")[0]
+                            output_path = Path(f"data/svg/{filename}.svg")
                             png_to_svg(cleaned_image_path, output_path)
+
                         except Exception as e:
                             logging.error(f"Error processing image {line['image']}: {e}")
                             traceback.print_exc()
